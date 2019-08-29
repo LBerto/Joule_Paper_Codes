447e987877572f3d9f26ff8110bdf73d7e49b10c
LBerto <38817610+LBerto@users.noreply.github.com>
2019-08-29 14:40:02 -0700

    Update physical_param.m

--- Data_simulations (Matlab)/physical_param.m
+++ Data_simulations (Matlab)/physical_param.m
@@ -4,7 +4,7 @@ function [] = physical_param()
 L = 8E-5;
 
 %Physical constants
-q=1.66*10^-19; %absolute value of the charge of the electron (in C)
+q=1.6*10^-19; %absolute value of the charge of the electron (in C)
 VT=0.026; %Thermal voltage at room temperature (in V)
 e0=8.85*10^-14; %Dielectric permittivity of the vacuum (in F.cm^-1)
 er=24; %Dielectric constant of the material
@@ -23,8 +23,8 @@ n0s=Nc*exp(-bn0/VT); %Surface concentration of electrons in x=0
 pLs=Nc*exp(-bpL/VT); %Surface concentration of holes in x=L
 
 %Optoelectronic parameters
-G = 1.485*2.5*10^21; %Generation rate (in cm^-3.s^-1)
 a = 2.5*10^4; %absorption coefficient (in cm^-1)
+G = a*1.485*10^17; %Generation rate (in cm^-3.s^-1)
 B = 5*10^-10; %Band to band recombination factor (in cm^3.s^-1)
 Sn = 10^5; %Electron surface recombination velocity at HTL (in cm.s^-1)
 Sp = 10^5; %Hole surface recombination velocity at ETL (in cm.s^-1)
